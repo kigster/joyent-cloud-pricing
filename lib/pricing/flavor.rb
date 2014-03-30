976586cc8e923c49be7088f9f2af070aa451fdb9
Konstantin Gredeskoul <pair+kig@wanelo.com>
2014-03-30 02:40:42 -0700

    Make it 1.9.3 compatible.. Whoops :(

--- lib/pricing/flavor.rb
+++ lib/pricing/flavor.rb
@@ -1,14 +1,13 @@
 module Joyent::Cloud::Pricing
   class Flavor
     attr_accessor :name, :os, :cost, :ram, :cpus, :disk
-
-    def initialize(name, os: os, cost: cost, ram: ram, cpus: cpus, disk: disk)
+    def initialize(name, options = {})
       @name = name
-      @os = os
-      @cost = (cost == 'N/A') ? nil : cost.to_f
-      @cpus = cpus.to_f
-      @disk = disk.to_i
-      @ram = ram.to_f
+      @os = options[:os]
+      @cost = (options[:cost] == 'N/A') ? nil : options[:cost].to_f
+      @cpus = options[:cpus].to_f
+      @disk = options[:disk].to_i
+      @ram = options[:ram].to_f
     end
 
     def to_h
